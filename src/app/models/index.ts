a5d55f452da9e33f47cebd360447a4e07e7e721c
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-11-24 21:34:30 -0300

    :construction: chore: models index file created
    connection config to the database

---
+++ src/app/models/index.ts
@@ -0,0 +1,33 @@
+import { Sequelize, Options } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
+interface ConfigProps extends Options {
+  host: string;
+  username: string;
+  password: string;
+  database: string;
+  dialect: "mysql" | "postgres" | "sqlite" | "mariadb" | "mssql" | undefined;
+}
+
+const config: ConfigProps = {
+  host: String(process.env.DB_HOST),
+  username: String(process.env.DB_USER),
+  password: String(process.env.DB_PASS),
+  database: String(process.env.DB_NAME),
+  dialect: process.env.NODE_ENV === "test" ? "sqlite" : "mysql",
+  storage: "./tests/database.sqlite",
+  define: {
+    timestamps: false,
+  },
+};
+
+export const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
