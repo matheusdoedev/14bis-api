21d8aef150f88a032c5bac7a9da22b75f9d106c3
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-11 17:53:01 -0300

    :wrench: chore: authentication route added

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,12 @@
 import express from "express";
 // controllers
 import UsersController from "../app/controllers/UserController";
+import SessionController from "../app/controllers/SessionController";
 // Router
 const routes = express.Router();
+
+// post - authentication
+routes.post("/authentication", SessionController.postUserAuthentication);
 
 // post - user create
 routes.post("/", UsersController.postUserCreate);
