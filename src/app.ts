eef9cf385cb3cf838742b231a6f2d03f91db25f0
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-11 17:52:44 -0300

    :wrench: chore: startup routes added/imported

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 // routes
 import usersRoutes from "./routes/users";
+import startupsRoutes from "./routes/startups";
 
 dotenv.config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
@@ -15,5 +16,6 @@ app.use(express.json());
 
 // routes
 app.use("/users", usersRoutes);
+app.use("/startups", startupsRoutes);
 
 export default app;
