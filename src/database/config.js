61ec159e30f2e1f2c0152781943a5302e68aec7e
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-03-01 13:44:58 -0300

    :wrench: chore: some config changes

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
 module.exports = {
@@ -7,8 +7,8 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT || "mysql",
-  storage: "./tests/database.sqlite",
+  dialect: process.env.DB_DIALECT || 'mysql',
+  storage: './tests/database.sqlite',
   define: {
     timestamps: false,
   },
